187056336fabb43ee23261118becf529b61b3eb8
AditiGi <aditisingh20144@gmail.com>
2024-02-24 00:24:21 +0530

    zjlc

---
+++ quizApp/script.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector("#button");
+const qstn = document.querySelector("#questions");
+
+btn.addEventListener("click", function () {
+  for (let i = 0; (i) => questions.length; i++) {}
+});
